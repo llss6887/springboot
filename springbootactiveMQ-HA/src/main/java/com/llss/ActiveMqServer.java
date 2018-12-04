f9f8b83b12fa6d55fdea1c14c1b11327dbbb5f04
kj <168337573@qq.com>
2018-12-04 15:56:40 +0800

    1111

---
+++ springbootactiveMQ-HA/src/main/java/com/llss/ActiveMqServer.java
@@ -0,0 +1,36 @@
+package com.llss;
+
+
+import com.llss.produce.ActiveMQProducer;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.Destination;
+
+@SpringBootApplication
+@RestController
+public class ActiveMqServer {
+
+    @Value("${messages.queue}")
+    private String queue;
+
+    @Autowired
+    private ActiveMQProducer activeMQProducer;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveMqServer.class, args);
+    }
+
+
+    @GetMapping("/msg/{str}")
+    public void set_queue(@PathVariable String str){
+        Destination amq = new ActiveMQQueue(queue);
+        activeMQProducer.send(amq, str);
+    }
+}
---
+++ springbootactiveMQ-HA/src/main/java/com/llss/consumer/ActiveMQConsumer.java
@@ -0,0 +1,18 @@
+package com.llss.consumer;
+
+
+import com.llss.produce.ActiveMQProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMQConsumer {
+    @Autowired
+    private ActiveMQProducer activeMQProducer;
+
+    @JmsListener(destination = "my_queue")
+    public void distribute(String json){
+        System.out.println("------------------------------------------------------------------------------------------------------這是我的集群"+json);
+    }
+}
